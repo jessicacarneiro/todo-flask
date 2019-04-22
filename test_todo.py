a49d82a501450b11db9c331112d0a4f1bd32dec3
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2019-04-22 18:24:25 -0300

    Listing tasks as a list.

--- test_todo.py
+++ test_todo.py
@@ -5,3 +5,15 @@ def test_list_tasks_should_return_200():
     with app.test_client() as client:
         response = client.get('/tasks')
         assert response.status_code == 200
+
+def test_list_tasks_should_be_json():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        response = client.get('/tasks')
+        assert response.content_type == 'application/json'
+
+def test_list_tasks_when_empty_should_return_empty_list():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        response = client.get('/tasks')
+        assert response.json == []
--- todo.py
+++ todo.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask('TodoApp')
 
 @app.route('/tasks')
 def list():
-    return ''
+    return jsonify([])
