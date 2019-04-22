a58bf5b239c9b4101c84644ad032fc6ad5686863
Jéssica Carneiro <jessicacarneiro@dcc.ufmg.br>
2019-04-22 19:09:33 -0300

    Adding a new task.

--- test_todo.py
+++ test_todo.py
@@ -1,4 +1,5 @@
 from todo import app, tasks
+import json
 
 def test_list_tasks_should_return_200():
     app.config['TESTING'] = True
@@ -30,3 +31,34 @@ def test_list_tasks_nonempty_returns_content():
             'status': False
         }]
     tasks.clear()
+
+
+def test_create_task_with_post():
+    with app.test_client() as client:
+        response = client.post('/task', data=json.dumps({
+        'title': 'task 1',
+        'description': 'my first task'}),
+        content_type='application/json')
+        assert response.status_code != 405
+
+
+def test_create_task_returns_new_task():
+    tasks.clear()
+    client = app.test_client()
+    response = client.post('/task', data=json.dumps({
+        'title': 'task 1',
+        'description': 'my first task'}),
+        content_type='application/json')
+    data = json.loads(response.data.decode('utf-8'))
+    assert data['id'] == 1
+    assert data['title'] == 'task 1'
+    assert data['description'] == 'my first task'
+    assert data['status'] is False
+
+def test_create_task_should_return_201():
+    with app.test_client() as client:
+        response = client.post('/task', data=json.dumps({
+            'title': 'task 1',
+            'description': 'my first task'}),
+            content_type='application/json')
+        assert response.status_code == 201
--- todo.py
+++ todo.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 app = Flask('TodoApp')
 
@@ -7,3 +8,15 @@ tasks = []
 @app.route('/tasks')
 def list():
     return jsonify(tasks)
+
+@app.route('/task', methods=['POST'])
+def add():
+    title = request.json.get('title')
+    description = request.json.get('description')
+    task = {
+        'id': len(tasks) + 1,
+        'title': title,
+        'description': description,
+        'status': False
+    }
+    return jsonify(task), 201
